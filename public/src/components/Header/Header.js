89bd8af3cf993dab9456e9615391c040891982e2
Naveen <naveen.kumarv@zoom.us>
2025-04-14 07:07:10 +0530

    header + signin fixed

--- public/src/components/Header/Header.js
+++ public/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./Header.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { uiAction } from "../../store/ui-gemini";
@@ -40,6 +41,10 @@ const Header = () => {
     window.open(continueWithOktaOauth(), "_self");
   };
 
+  const getFirstName = (fullName) => {
+    return fullName?.split(" ")[0] || "User";
+  };
+
   return (
     <div className={styles["header-main"]}>
       <div className={styles["left-section"]}>
@@ -60,22 +65,34 @@ const Header = () => {
           </div>
         ) : null}
 
-        {isLogin ? (
-          <div
-            onClick={userDetailsOpen}
-            className={`${styles["user"]} ${
-              isUserDetails ? styles["clicked-user"] : ""
-            }`}>
-            {userDetails?.profileImg.length > 0 ? (
-              <img src={userDetails?.profileImg} alt=""></img>
-            ) : (
-              ""
-            )}
+        {!isLogin ? (
+          <div className={styles["login"]} onClick={loginHandler}>
+            <div className={styles.zoomLogo}>Z</div>
+            <p>Sign in</p>
           </div>
         ) : (
-          <div className={styles["login"]} onClick={loginHandler}>
-            <img src={commonIcon.oktaLogo} alt="okta logo"></img>
-            <p>Sign in with Okta</p>
+          <div className={styles.userSection}>
+            <div className={styles.userName}>
+              {getFirstName(userDetails?.name)}
+            </div>
+            <div
+              onClick={userDetailsOpen}
+              className={`${styles["user"]} ${
+                isUserDetails ? styles["clicked-user"] : ""
+              }`}>
+              {userDetails?.profileImg ? (
+                <img src={userDetails.profileImg} alt={userDetails.name} />
+              ) : (
+                <div className={styles.defaultAvatar}>
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path
+                      d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V20H20V18C20 15.34 14.67 14 12 14Z"
+                      fill="#0B5CFF"
+                    />
+                  </svg>
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
